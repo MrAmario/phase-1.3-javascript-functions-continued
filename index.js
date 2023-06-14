04a34a2fb25b19864698cf0814be8a1e36e82d45
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-06-14 14:38:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(leisure = "roller-skate"){
+    
+    return (`This Saturday, I want to ${leisure}!`)
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+mondayWork=function (work = "go to the office"){
+     return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(wrapper = "*"){
+    return function emphaticOne(adjective){
+        //console.log=`You are ${wrapper}${adjective}${wrapper}!`
+        return (`You are ${wrapper}${adjective}${wrapper}!`)
+        
+    }
+}
